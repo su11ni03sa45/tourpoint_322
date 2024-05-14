de4f9880d2c7d7a75975611219c5cae405edb349
su11ni03sa45 <158018833+su11ni03sa45@users.noreply.github.com>
2024-05-14 16:35:52 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,200 @@
+var tourpoint = {
+      "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Khao Yai National Park",
+              "type": "natural",
+              "level": 5
+            },
+            "geometry": {
+              "coordinates": [
+                101.49971039799152,
+                14.332675336860973
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Grand Palace",
+              "type": "cutural",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.49160303762392,
+                13.750977105286609
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Iconsaiam",
+              "type": "spacial",
+              "level": 3
+            },
+            "geometry": {
+              "coordinates": [
+                100.51020046560888,
+                13.726678148489398
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "future park rangsit",
+              "type": "spacial",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.61764530555621,
+                13.988881511937734
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Chatuchak Market",
+              "type": "market",
+              "level": 5
+            },
+            "geometry": {
+              "coordinates": [
+                100.55051143121136,
+                13.799852989111628
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Wachirabenchathat Park",
+              "type": "park",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.55415185835449,
+                13.812790446053882
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Wat Arun",
+              "type": "wat",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.48847761188478,
+                13.74380869960919
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Taling Chan Floating ",
+              "type": "market",
+              "level": 3
+            },
+            "geometry": {
+              "coordinates": [
+                100.45630127116232,
+                13.7768395380984
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Bankkok Art",
+              "type": "spacial",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.53024879294736,
+                13.746764990234396
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Giant Swing",
+              "type": "spacial",
+              "level": 3
+            },
+            "geometry": {
+              "coordinates": [
+                100.50126646122897,
+                13.75182219135229
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Sky Garden",
+              "type": "cutural",
+              "level": 3
+            },
+            "geometry": {
+              "coordinates": [
+                100.49857700932705,
+                13.739101279621792
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Talat Phlu",
+              "type": "market",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.47788228388339,
+                13.720951937206877
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Bank of Thailand Learing Center",
+              "type": "spacial",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.49807529341405,
+                13.76762409037805
+              ],
+              "type": "Point"
+            }
+          }
+        ]
+      }
